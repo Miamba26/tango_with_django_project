b11817751d08c77612d9acc3102cf7ab1ea37efd
Ibrahim Asghar <2617847a@student.gla.ac.uk>
2024-01-24 22:07:24 +0000

    Chapter 5.3

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=model.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    view = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
