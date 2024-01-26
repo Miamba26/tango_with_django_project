452facd8289c42717b43f78b2bf476db77e49eb8
Ibrahim Asghar <2617847a@student.gla.ac.uk>
2024-01-26 15:07:02 +0000

    Chapter 6 complete

--- populate_rango.py
+++ populate_rango.py
@@ -16,32 +16,32 @@ def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/',
-         'views': 128, 'likes': 64},
+         'views': 128},
         {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/',
-         'views': 128, 'likes': 64},
+         'views': 128},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/',
-         'views': 128, 'likes': 64} ]
+         'views': 128} ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-         'views': 64, 'likes': 32},
+         'views': 64},
         {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/',
-         'views': 64, 'likes': 32},
+         'views': 64},
         {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/',
-         'views': 64, 'likes': 32} ]
+         'views': 64} ]
     
     other_pages = [
         {'title':'Bottle',
          'url':'http://bottlepy.org/docs/dev/',
-         'views': 32, 'likes': 16},
+         'views': 32},
         {'title':'Flask',
          'url':'http://flask.pocoo.org',
-         'views': 32, 'likes': 16} ]
+         'views': 32} ]
     
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64}, 
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32}, 
